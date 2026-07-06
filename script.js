15cea804b3d2f4995d9b8ad9130f9be980eff3fa
sihooneunbin <eunbin1231@gmail.com>
2026-07-06 16:56:40 +0900

    Update script.js

--- script.js
+++ script.js
@@ -40,3 +40,20 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
 });
+const fades = document.querySelectorAll(".fade");
+
+const observer = new IntersectionObserver((entries)=>{
+
+    entries.forEach(entry=>{
+
+        if(entry.isIntersecting){
+
+            entry.target.classList.add("show");
+
+        }
+
+    });
+
+});
+
+fades.forEach(item=>observer.observe(item));

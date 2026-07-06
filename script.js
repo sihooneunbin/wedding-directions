81b1eacf9002888262b798354672888a8a1efc14
sihooneunbin <eunbin1231@gmail.com>
2026-07-06 15:41:13 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+const copyButton = document.getElementById("copyAddress");
 
+copyButton.addEventListener("click", function(e){
+
+    e.preventDefault();
+
+    navigator.clipboard.writeText(
+        "부산광역시 해운대구 센텀중앙로 79 센텀사이언스파크"
+    );
+
+    alert("주소가 복사되었습니다.");
+
+});

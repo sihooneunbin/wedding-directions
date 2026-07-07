3e5f30c843bbc179344aa60b765020607f408dd6
sihooneunbin <eunbin1231@gmail.com>
2026-07-07 09:45:47 +0900

    Update script.js

--- script.js
+++ script.js
@@ -64,3 +64,30 @@ window.addEventListener("load", function(){
     window.scrollTo(0,0);
 
 });
+const topButton = document.getElementById("topButton");
+
+window.addEventListener("scroll", () => {
+
+    if(window.scrollY > 300){
+
+        topButton.classList.add("show");
+
+    }else{
+
+        topButton.classList.remove("show");
+
+    }
+
+});
+
+topButton.addEventListener("click", () => {
+
+    window.scrollTo({
+
+        top:0,
+
+        behavior:"smooth"
+
+    });
+
+});
